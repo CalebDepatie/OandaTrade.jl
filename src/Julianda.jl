0a5209432b5703ec2ff2bb57f7895c4e1c3f64fc
Caleb Depatie <aloysuiswolf@gmail.com>
2019-12-14 22:40:47 -0500

    Accounts endpoint
    Added Position struct

--- src/Julianda.jl
+++ src/Julianda.jl
@@ -1,11 +1,11 @@
 module Julianda
 
-include("Account.jl")
-include("Config.jl")
-include("Instrument.jl")
 include("Order.jl")
 include("Position.jl")
 include("Trade.jl")
+include("Account.jl")
+include("Config.jl")
+include("Instrument.jl")
 include("Transaction.jl")
 
 end # module
--- src/Trade.jl
+++ src/Trade.jl
@@ -1,3 +1,15 @@
 module Trade
 
+"Detailed Trade struct from Oanda"
+struct trade
+
+end
+
+"Turns the raw dict data into trades"
+function tradeDictToStruct(posDict)
+    trades = []
+
+    return trades
+end
+
 end
