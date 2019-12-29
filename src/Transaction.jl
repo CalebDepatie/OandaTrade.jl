dfa16152df140fefe130e2625c0f01510bde3bdd
lsaenzt <59137624+lsaenzt@users.noreply.github.com>
2019-12-29 20:09:01 +0100

    Instruments (#3)
    * Create settings.json
    
    * commit
    
    * Added orderBook and PositionBook Endpoints
    
    * commit
    
    * changes in gitignore
    
    * Delete Manifest.toml
    
    * gitignore vscode
    
    * Update Transaction.jl
    
    * Added keyword arguments functionality to getcandles methods
    
    * updated gitignore

--- src/Transaction.jl
+++ src/Transaction.jl
@@ -1,3 +1,21 @@
 module Transaction
 
+using HTTP, JSON3, Dates, CodecZlib
+#------------------------------------------------------------------------------------
+#/accounts/{accountID}/transactions Endpoint
+#------------------------------------------------------------------------------------
+
+
+#------------------------------------------------------------------------------------
+#/accounts/{accountID}/transactions/{transactionID} Endpoint
+#------------------------------------------------------------------------------------
+mutable struct transaction
+    id
+    time
+    userID
+    accountID
+    batchID
+    requestID
+    type
+    units
 end
