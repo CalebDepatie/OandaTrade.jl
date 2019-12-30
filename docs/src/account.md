abdbf2608c6c55dccc6d08af768614391fc76d8e
Caleb Depatie <aloysuiswolf@gmail.com>
2019-12-30 16:45:19 -0500

    Documentation
    Adding Documenter based documentation and full docstrings

---
+++ docs/src/account.md
@@ -0,0 +1,22 @@
+# Accounts
+
+```@meta
+CurrentModule = Julianda.Account
+```
+
+## Functions
+
+```@docs
+Account.listAccounts
+Account.getAccount
+Account.getAccountSummary
+Account.getAccountInstruments
+Account.setAccountConfig
+```
+
+## Structs
+
+```@docs
+Account.account
+Account.instrumentDetail
+```
--- test/pricing_test.jl
+++ test/pricing_test.jl
@@ -1,8 +1,19 @@
-using Test
+using Test, Dates
 
 using Julianda
 
 foo = Julianda.Config.loadConfig("../config")
 
-# Keeps asking for price even if the market is closed
-#@test Julianda.Pricing.getPrice(foo, "GBP_USD")
+# Checks for the right response depending on the time
+dt = Dates.now()
+if Dates.dayofweek(dt) >= 5
+    if Dates.dayofweek(dt) == 5 & Dates.hour(dt) < 4
+        @test typeof(Julianda.Pricing.getPrice(foo, "GBP_USD")) == Vector{Julianda.Pricing.price}
+    elseif Dates.dayofweek(dt) == 7 & Dates.hour(dt) >= 5
+        @test typeof(Julianda.Pricing.getPrice(foo, "GBP_USD")) == Vector{Julianda.Pricing.price}
+    else
+        @test_throws Julianda.Pricing.ClosedMarketException Julianda.Pricing.getPrice(foo, "GBP_USD")
+    end
+else
+    @test typeof(Julianda.Pricing.getPrice(foo, "GBP_USD")) == Vector{Julianda.Pricing.price}
+end
