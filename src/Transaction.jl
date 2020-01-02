c58cfa9f0aa164805fc4332d57c7f6591b4c2d23
Luis Sáenz <lsaenzt@gmail.com>
2020-01-02 23:03:37 +0100

    Added transactionID, transactions/idrange, transaction/sinceid  endpoints

--- src/Transaction.jl
+++ src/Transaction.jl
@@ -24,21 +24,19 @@ function coerceTransactionPages(tpages::transactionPages)
 
     RFC = Dates.DateFormat("yyyy-mm-ddTHH:MM:SS.sssssssssZ")
 
-    tpages.from = DateTime(tpages.from[1:23], RFC) #DateTime has milliseconds precision. Can only use 23 characters...
-    tpages.to = DateTime(tpages.to[1:23], RFC)
+    tpages.from = DateTime(first(tpages.from,23), RFC) #DateTime has milliseconds precision. Can only use 23 characters...
+    tpages.to = DateTime(first(tpages.to,23), RFC)
     tpages.lastTransactionID = parse(Int,tpages.lastTransactionID)
 
     return tpages
-
 end
 
 """
     getTransactionPages(config::config; from::DateTime=nothing, to::DateTime=Dates.now(), pageSize::Int=100, type::String=nothing)
 
-    Returns a struct. Field :pages includes the urls for getting the transactions in the given timeframe
+Return a struct. Field ':pages' includes the urls for requesting the transactions data in the given timeframe
 
 # Example
-
     getTransactionPages(userdata, from=DateTime(2019,5,31),type="MARKET_ORDER,STOP_LOSS_ORDER")
 """
 function getTransactionPages(config; from::Union{DateTime,Nothing}=nothing, to::DateTime=Dates.now(), pageSize::Int=100, type::Union{String,Nothing}=nothing)
@@ -50,42 +48,126 @@ function getTransactionPages(config; from::Union{DateTime,Nothing}=nothing, to::
     r = HTTP.get(string("https://", config.hostname, "/v3/accounts/", config.account, "/transactions"),
     ["Authorization" => string("Bearer ", config.token),"Accept-Datetime-Format" => "RFC3339"]; query = q)
 
-    if r.status != 200
-        println(r.status)
-    end
-
     return JSON3.read(r.body,transactionPages) |> coerceTransactionPages
 end
 #------------------------------------------------------------------------------------
 #/accounts/{accountID}/transactions/{transactionID} Endpoint
 #------------------------------------------------------------------------------------
 mutable struct transaction
-    transaction
+    transaction::Dict{String,Any}
     lastTransactionID
 
     transaction() = new()
 end
 
-function getTransaction(config::config,tID::Int)
+# Declaring JSON3 struct types
+JSON3.StructType(::Type{transaction}) = JSON3.Mutable()
+
+"Recursive coersion of transaction Dictionaries to proper Julia types"
+function coerceTransactionDict(tDict::Dict{String,Any}) #Also used in getTransactions endpoints
+
+    valueFields = ["price","priceBound","fullVWAP","distance","closeoutBid","closeoutAsk",
+                   "marginRate","initialMarginRequired","requestedUnits","liquidity",
+                   "gainQuoteHomeConversionFactor","lossQuoteHomeConversionFactor",
+                   "AccountBalance","amount","financing","pl","commission","guaranteedExecutionFee",
+                   "halfSpreadCost","dividend","realizedPL","bidLiquidityUsed","askLiquidityUsed"]
+
+    timeFields = ["timestamp","time","gtdTime"]
+    RFC = Dates.DateFormat("yyyy-mm-ddTHH:MM:SS.sssssssssZ")
+
+    for (key,value) in tDict
+        if eltype(value) == Pair{String,Any}
+            tDict[key] = coerceTransactionDict(tDict[key]) #Recursion for Dictionaries inside a transaction field
+        elseif eltype(value) == Any #'asks' & 'bids' in 'fullPrice' has an array of liquidity,prices
+            tDict[key]=collect(coerceTransactionDict.(tDict[key]))
+        elseif key in valueFields
+            tDict[key] = parse(Float32,value)
+        elseif key=="units" && value != "ALL"
+            tDict[key] = parse(Float32,value)
+        elseif key in timeFields
+            tDict[key] = DateTime(first(value,23),RFC)
+        end
+    end
+
+    return tDict
+end
+
+"""
+    getTransactios(config::config, tID::Int)
+
+Return a Dictionary with the transaction data
+
+# Example
+    getTransaction(userdata,4)
+"""
+function getTransaction(config::config, tID::Int)
     
+    r = HTTP.get(string("https://", config.hostname, "/v3/accounts/", config.account, "/transactions/",string(tID)),
+    ["Authorization" => string("Bearer ", config.token),"Accept-Datetime-Format" => "RFC3339"])
+
+    temp = JSON3.read(r.body,transaction)
+    temp.transaction = coerceTransactionDict(temp.transaction)
+    return temp
 end
 #------------------------------------------------------------------------------------
 #/accounts/{accountID}/transactions/idrange  Endpoint
 #/accounts/{accountID}/transactions/sinceid  Endpoint
 #------------------------------------------------------------------------------------
+"""
+    getTransactions(config::config, fromID::Int, toID::Int, type::Union{String,Nothing}=nothing)
+    getTransactions(config::config, sinceID::Int, type::Union{String,Nothing}=nothing)
+
+Return an array of Dictionaries with the transactions data
+
+# Examples
+    getTransactions(userdata,3,"MARKET_ORDER,STOP_LOSS_ORDER")
+    getTransactions(userdata,2,10)
+"""
 mutable struct transactions
-    transactions
+    transactions::Array{Dict{String,Any},1}
     lastTransactionID
 
     transactions() = new()
 end
 
-function getTransactions(config::config,fromID::Int,toID::Int)
+# Declaring JSON3 struct types
+JSON3.StructType(::Type{transactions}) = JSON3.Mutable()
+
+function getTransactions(config::config,fromID::Int,toID::Int,type::Union{String,Nothing}=nothing)
     
+    q= Dict("from" => string(fromID),"to" => string(toID))
+    !isnothing(type) && push!(q,"type"=>type)
+
+    r = HTTP.get(string("https://", config.hostname, "/v3/accounts/", config.account, "/transactions/idrange"),
+    ["Authorization" => string("Bearer ", config.token),"Accept-Datetime-Format" => "RFC3339"]; query = q)
+    
+    temp = JSON3.read(r.body,transactions)
+
+    for dict in temp.transactions
+        dict = coerceTransactionDict(dict)
+    end
+    
+    return temp
 end
 
-function getTransactions(config::config,sinceID::Int)
+function getTransactions(config::config,sinceID::Int,type::Union{String,Nothing}=nothing)
+
+    q= Dict("id" => string(sinceID))
+    !isnothing(type) && push!(q,"type"=>type)
+
+    r = HTTP.get(string("https://", config.hostname, "/v3/accounts/", config.account, "/transactions/sinceid"),
+    ["Authorization" => string("Bearer ", config.token),"Accept-Datetime-Format" => "RFC3339"]; query = q)
+
+    temp = JSON3.read(r.body,transactions)
+
+    for dict in temp.transactions
+        dict = coerceTransactionDict(dict)
+    end
     
+    return temp
 end
+#------------------------------------------------------------------------------------
+#/accounts/{accountID}/transactions/stream Endpoint
+#------------------------------------------------------------------------------------
 
 end #Module
