837791b0ad0b519d0e6d12a34efef422e52ad1f9
Caleb Depatie <aloysuiswolf@gmail.com>
2020-01-14 18:05:21 -0500

    Positions
    - Continued with the positions endpoint
    - Various fixes

--- src/Position.jl
+++ src/Position.jl
@@ -31,6 +31,13 @@ mutable struct positionTopLayer
     positionTopLayer() = new()
 end
 
+"Top layer for positions endpoint"
+mutable struct positionTopLayerSingle
+    position::position
+
+    positionTopLayerSingle() = new()
+end
+
 "Coerce a given Position into its proper types (Used internally)"
 function coercePos(pos::position)
     pos.long.pl = parse(Float32, pos.long.pl)
@@ -53,6 +60,7 @@ end
 # Declaring JSON3 struct types
 JSON3.StructType(::Type{Position.position}) = JSON3.Mutable()
 JSON3.StructType(::Type{Position.positionTopLayer}) = JSON3.Mutable()
+JSON3.StructType(::Type{Position.positionTopLayerSingle}) = JSON3.Mutable()
 JSON3.StructType(::Type{Position.posData}) = JSON3.Mutable()
 
 """
@@ -78,6 +86,51 @@ function listPositions(config)
     end
 
     return temp
+end
+
+"""
+    listOpenPositions(config)
+
+Returns a list of current positions that have an open trade
+"""
+function listOpenPositions(config)
+    r = HTTP.request("GET", string("https://", config.hostname,
+                    "/v3/accounts/", config.account, "/openPositions"),
+    ["Authorization" => string("Bearer ", config.token),
+    "Accept-Datetime-Format" => config.datetime])
+    if r.status != 200
+        println(r.status)
+    end
+    data = JSON3.read(r.body, positionTopLayer)
+    data = data.positions
+
+    temp = Vector{position}()
+    for pos in data
+        pos = coercePos(pos)
+        push!(temp, pos)
+    end
+
+    return temp
+end
+
+"""
+    getPosition(config, instrument)
+
+Returns position data for a specified instrument
+"""
+function getPosition(config, instrument)
+    r = HTTP.request("GET", string("https://", config.hostname,
+                    "/v3/accounts/", config.account, "/positions/", instrument),
+    ["Authorization" => string("Bearer ", config.token),
+    "Accept-Datetime-Format" => config.datetime])
+    if r.status != 200
+        println(r.status)
+    end
+    data = JSON3.read(r.body, positionTopLayerSingle)
+
+    data = coercePos(data.position)
+
+    return data
 end
 
 end
