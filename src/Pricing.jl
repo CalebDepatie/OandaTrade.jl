cd8f6bf2e3ab1efe8c5c3a912c1364bb8f2a77f5
lsaenzt <59137624+lsaenzt@users.noreply.github.com>
2020-02-19 16:38:56 +0100

    getPrice and StreamPrice endpoints

--- src/Pricing.jl
+++ src/Pricing.jl
@@ -58,29 +58,24 @@ function coercePrice(price::price)
     return price
 end
 
-"Exception thrown when the market is closed on the weekend"
-struct ClosedMarketException <: Exception end
-
 """
     function getPrice(config, instruments)
 
 Get the most recent price update of an instrument
 
+# Arguments
+    - 'config::config': a valid struct with user configuracion data
+    - 'instruments::Vector{String}': a vector of valid pairs (e.g. ["EUR_USD","EUR_JPY"])
 
+#Example
+
+    getPrice(userconfig, ["EUR_USD","EUR_JPY"])
+    
 """
 function getPrice(config::config, instruments::Vector{String})
-    #= Do we need to check this every time? 
-    dt = Dates.now()
-    if Dates.dayofweek(dt) >= 5  
-        if Dates.dayofweek(dt) == 5 & Dates.hour(dt) < 4
-        elseif Dates.dayofweek(dt) == 7 & Dates.hour(dt) >= 5
-        else
-            throw(ClosedMarketException())
-        end
-    end
-    =#
+
     r = HTTP.get(string("https://", config.hostname, "/v3/accounts/",config.account,"/pricing?instruments=", join(instruments,",")),
-                    ["Authorization" => string("Bearer ", config.token), "Accept-Datetime-Format" => config.datetime,],)
+                ["Authorization" => string("Bearer ", config.token), "Accept-Datetime-Format" => config.datetime,],)
 
     data = JSON3.read(r.body, priceTopLayer)
 
@@ -89,11 +84,38 @@ function getPrice(config::config, instruments::Vector{String})
     end
 
     return data.prices #Does not return 'since' datetime
-end
 
+end
 # ------------------------------------------------------------------------------------
 # /accounts/{accountID}/pricing/stream Endpoint
 # ------------------------------------------------------------------------------------
+"""
+    function streamprice(f, config , instruments)
+
+Returns a stream of price objects and apply a function to each one of them
+
+# Arguments
+    - 'f::Function': a function to apply to each price struct object. Streamprice accepts do block format
+    - 'config::config': a valid struct with user configuracion data
+    - 'instruments::Vector{String}': a vector of valid pairs (e.g. ["EUR_USD","EUR_JPY"])
+
+#Example
+
+    streamprice(userconfig, ["EUR_JPY"]) do price
+        println(price)
+    end
+"""
+function streamPrice(f::Function, config::config,instruments::Vector{String})
+
+@async HTTP.open("GET", 
+        string("https://", config.streamingHostname, "/v3/accounts/",config.account,"/pricing/stream?instruments=", join(instruments,",")),
+        ["Authorization" => string("Bearer ", config.token)]) do io
+            for line in eachline(io)
+                p = JSON3.read(line, price)
+                p.type != "HEARTBEAT" && f(p) #Cleans HEARTBEAT ticks so that only prices are sent to f
+            end
+        end
+end
 
 
 # ------------------------------------------------------------------------------------
