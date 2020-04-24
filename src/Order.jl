ad76dcb79228e45398e422a451f105be7bfb2c46
lsaenzt <59137624+lsaenzt@users.noreply.github.com>
2020-04-24 17:13:38 +0200

    Improved documentation

--- src/Order.jl
+++ src/Order.jl
@@ -57,6 +57,7 @@ end
 "Detailed OrderRequest struct from Oanda"
 mutable struct orderRequest
     clientExtensions::clientExtensions 
+    distance # for orders on fill
     gtdTime
     instrument # instrument of the order
     positionFill # Type of position fill on the order
@@ -66,6 +67,7 @@ mutable struct orderRequest
     takeProfitOnFill::takeProfit
     timeInForce # Type of time in force
     tradeClientExtensions::clientExtensions
+    tradeID::String
     trailingStopLossOnFill::trailingStopLoss
     triggerCondition # Trigger condition of the order
     type # Type of order
@@ -91,22 +93,35 @@ JSON3.StructType(::Type{trailingStopLoss}) = JSON3.Mutable()
 JSON3.omitempties(::Type{trailingStopLoss})=(:price,:timeInForce,:gtdTime)
 
 JSON3.StructType(::Type{orderRequest}) = JSON3.Mutable()
-JSON3.omitempties(::Type{orderRequest})=(:price, :units, :priceBound,:triggerCondition,:gtdTime,
+JSON3.omitempties(::Type{orderRequest})=(:price, :units, :distance, :priceBound,:triggerCondition,:gtdTime,
                                          :takeProfitOnFill,:stopLossOnFill,:trailingStopLossOnFill,
-                                         :clientExtensions,:tradeClientExtensions)
+                                         :clientExtensions,:tradeID, :tradeClientExtensions)
 
 JSON3.StructType(::Type{order2send}) = JSON3.Struct()
 
 # market order -----------------------------------------------------------------------
 """
- marketOrder(config, instrument, units;[TIF, positionFill, priceBound, TP ,SL ,tSL, clientExt ,tradeExt])
+    marketOrder(config, instrument, units;[TIF, positionFill, priceBound, TP ,SL ,tSL, clientExt ,tradeExt])
+
+Places a market order
+
+#Arguments
+- 'config::config': a valid struct with user configuration data
+- 'instrument::String': a valid instrument (e.g. "EUR_USD")
+- 'units::String': units to buy (positive value) or sell (negative value)
+
+# Keyword Arguments
+- 'TIF::String' : timeinForce value ("GTC","GTD","GFD","FOK","IOC")
+- 'positionFill::String' : how positions are modified when the order is filled ("OPEN_ONLY", "REDUCE_FIRST", "REDUCE_ONLY","DEFAULT"). Defaults to "DEFAULT"
+- 'priceBound::String': worst price for filling
+- 'TP', 'SL', 'tSL' : a NamedTuple with order-on-fill details. Valid values are 'price::Real', 'distance::Real', 'timeInForce::String' and 'gtdTime::String' 
+- 'clientExt'(not implemented)
+- 'tradeExt' (not implemented)
 
 #Examples
 
    marketOrder(userData,"EUR_JPY",100)
-
-   marketOrder(userData,"EUR_CHF",100,SL=(distance=0.1,),TP=(price=1.12,),tSL=(distance=0.3,))
-
+   marketOrder(userData,"EUR_CHF",100,SL=(distance=0.1,),TP=(price=1.12,),tSL=(distance=0.3,)) #Do not forget the comma for single value NamedTuple
 """
 function marketOrder(config::config, instrument::String, units::Real;
                      TIF::String = "FOK", positionFill::String = "DEFAULT", priceBound::Union{Nothing,String}=nothing,
@@ -167,7 +182,6 @@ end
  nonMarketOrder(config, type, instrument, units, price;[TIF, gtdTime, positionFill, trigge, priceBound, TP ,SL ,tSL, clientExt ,tradeExt])
 
  generic order function for limit, stop and marketIfTouchedOrders
-
 """
 
 function nonMarketOrder(config::config, type::String, instrument::String, units::Real, price::Real;
@@ -229,14 +243,28 @@ end
 # limit order -----------------------------------------------------------------------
 """
 
- limitOrder(config, instrument, units, price;[TIF, positionFill, priceBound, TP ,SL ,tSL, clientExt ,tradeExt])
+    limitOrder(config, instrument, units, price;[TIF, positionFill, priceBound, TP ,SL ,tSL, clientExt ,tradeExt])
+ 
+Places a limit Order
+
+#Arguments
+- 'config::config': a valid struct with user configuration data
+- 'instrument::String': a valid instrument (e.g. "EUR_USD")
+- 'units::String': units to buy (positive value) or sell (negative value)
+- 'price::Real'
+
+# Keyword Arguments
+- 'TIF::String' : timeinForce value ("GTC","GTD","GFD","FOK","IOC")
+- 'positionFill::String' : how positions are modified when the order is filled ("OPEN_ONLY", "REDUCE_FIRST", "REDUCE_ONLY","DEFAULT"). Defaults to "DEFAULT"
+- 'priceBound::String': worst price for filling
+- 'TP', 'SL', 'tSL' : a NamedTuple with order-on-fill details. Valid values are 'price::Real', 'distance::Real', 'timeInForce::String' and 'gtdTime::String' 
+- 'clientExt'(not implemented)
+- 'tradeExt' (not implemented)
 
 #Examples
 
    limitOrder(userData,"EUR_USD",100, 1.10)
-
-   limitOrder(userData,"EUR_JPY",100,117,SL=(distance=1,),TP=(price=12,),tSL=(distance=3,))
-
+   limitOrder(userData,"EUR_JPY",100,117,SL=(distance=1,),TP=(price=12,),tSL=(distance=3,)) #Do not forget the comma for single value NamedTuple
 """
 limitOrder(config::config, instrument::String, units::Real, price::Real;
                     TIF::String = "GTC", gtdTime::Union{Nothing,String}=nothing, positionFill::String = "DEFAULT", trigger::String="DEFAULT",
@@ -248,15 +276,28 @@ limitOrder(config::config, instrument::String, units::Real, price::Real;
     
  # stop order -----------------------------------------------------------------------
 """
+    stopOrder(config, instrument, units, price;[TIF, positionFill, priceBound, TP ,SL ,tSL, clientExt ,tradeExt])
 
-stopOrder(config, instrument, units, price;[TIF, positionFill, priceBound, TP ,SL ,tSL, clientExt ,tradeExt])
+Places a stop Order
+
+#Arguments
+- 'config::config': a valid struct with user configuration data
+- 'instrument::String': a valid instrument (e.g. "EUR_USD")
+- 'units::String': units to buy (positive value) or sell (negative value)
+- 'price::Real'
+
+# Keyword Arguments
+- 'TIF::String' : timeinForce value ("GTC","GTD","GFD","FOK","IOC")
+- 'positionFill::String' : how positions are modified when the order is filled ("OPEN_ONLY", "REDUCE_FIRST", "REDUCE_ONLY","DEFAULT"). Defaults to "DEFAULT"
+- 'priceBound::String': worst price for filling
+- 'TP', 'SL', 'tSL' : a NamedTuple with order-on-fill details. Valid values are 'price::Real', 'distance::Real', 'timeInForce::String' and 'gtdTime::String' 
+- 'clientExt'(not implemented)
+- 'tradeExt' (not implemented)
 
 #Examples
 
   stopOrder(userData,"EUR_USD",100, 1.10)
-
-  stopOrder(userData,"EUR_JPY",100,117,SL=(distance=1,),TP=(price=12,),tSL=(distance=3,))
-
+  stopOrder(userData,"EUR_JPY",100,117,SL=(distance=1,),TP=(price=12,),tSL=(distance=3,)) #Do not forget the comma for single value NamedTuple
 """
 stopOrder(config::config, instrument::String, units::Real, price::Real;
                    TIF::String = "GTC", gtdTime::Union{Nothing,String}=nothing, positionFill::String = "DEFAULT", trigger::String="DEFAULT", priceBound::Union{Nothing,String}=nothing,
@@ -269,15 +310,28 @@ stopOrder(config::config, instrument::String, units::Real, price::Real;
 
 # market if touched order -----------------------------------------------------------------------
 """
-
 marketIfTouchedOrder(config, instrument, units, price;[TIF, positionFill, priceBound, TP ,SL ,tSL, clientExt ,tradeExt])
+
+Places a market-if-touched Order
+
+#Arguments
+- 'config::config': a valid struct with user configuration data
+- 'instrument::String': a valid instrument (e.g. "EUR_USD")
+- 'units::String': units to buy (positive value) or sell (negative value)
+- 'price::Real'
+
+# Keyword Arguments
+- 'TIF::String' : timeinForce value ("GTC","GTD","GFD","FOK","IOC")
+- 'positionFill::String' : how positions are modified when the order is filled ("OPEN_ONLY", "REDUCE_FIRST", "REDUCE_ONLY","DEFAULT"). Defaults to "DEFAULT"
+- 'priceBound::String': worst price for filling
+- 'TP', 'SL', 'tSL' : a NamedTuple with order-on-fill details. Valid values are 'price::Real', 'distance::Real', 'timeInForce::String' and 'gtdTime::String' 
+- 'clientExt'(not implemented)
+- 'tradeExt' (not implemented)
 
 #Examples
 
   marketifTouchedOrder(userData,"EUR_uSD",100, 1.10)
-
-  marketifTouchedOrder(userData,"EUR_JPY",100,117,SL=(distance=1,),TP=(price=12,),tSL=(distance=3,))
-
+  marketifTouchedOrder(userData,"EUR_JPY",100,117,SL=(distance=1,),TP=(price=12,),tSL=(distance=3,)) #Do not forget the comma for single value NamedTuple
 """
 marketIfTouchedOrder(config::config, instrument::String, units::Real, price::Real;
                    TIF::String = "GTC", gtdTime::Union{Nothing,String}=nothing, positionFill::String = "DEFAULT", trigger::String="DEFAULT", priceBound::Union{Nothing,String}=nothing,
@@ -286,7 +340,46 @@ marketIfTouchedOrder(config::config, instrument::String, units::Real, price::Rea
        nonMarketOrder(config, "MARKET_IF_TOUCHED", instrument, units, price; 
                   TIF=TIF, gtdTime=gtdTime, positionFill=positionFill, trigger=trigger, priceBound = priceBound,
                   TP=TP, SL=SL ,tSL=tSL, clientExt=clientExt, tradeExt=tradeExt)
-                 
+    
+                  
+ # related Orders (Stop loss, take profit, trailing stop loss) -----------------------------------------------------------------------
+ 
+ """
+     relatedOrder(config, tradeID, type, detail)
+ 
+ Places a stop loss, take profit or trailing stop loss to an open trade (not order)
+
+#Arguments
+- 'config::config': a valid struct with user configuration data
+- 'tradeID::Union{Int,String}': a valid open trade ID (e.g. "123")
+- 'type::String' : type of the order "STOP_LOSS", "TAKE_PROFIT" or "TRAILING_STOP_LOSS"
+- 'details::NamedTuple': a NamedTuple with order-on-fill details. Valid values are 'price::Real', 'distance::Real', 'timeInForce::String' and 'gtdTime::String' 
+
+ #Examples
+ 
+    relatedOrder(config, "156", "STOP_LOSS", details=(distance=0.1,)) #Do not forget the comma for single value NamedTuple 
+ """
+function relatedOrder(config, tradeID::Union{Int,String}, type::String, details::NamedTuple=NamedTuple() )
+
+    o = orderRequest()
+    
+    o.type = type
+    o.tradeID = string(tradeID)
+    o.timeInForce = "GTC"
+  
+    haskey(details, :price) && (o.price = details.price)
+    haskey(details, :distance) && (o.distance = details.distance)
+    haskey(details, :timeInForce) && (o.timeInForce = details.timeInForce)
+    haskey(details, :gtdTime) && (o.price = Dates.format(details.gtdTime,"yyyy-mm-ddTHH:MM:SS.sss000000Z"))
+
+    data = order2send(o)
+
+    r = HTTP.post(string("https://",config.hostname,"/v3/accounts/",config.account,"/orders",),
+        ["Authorization" => string("Bearer ", config.token), "Content-Type" => "application/json", ],
+        JSON3.write(data),)
+
+    return JSON3.read(r.body,Dict{String,Any})  
+end
 
 # ------------------------------------------------------------------------------------
 # /accounts/{accountID}/orders GET Endpoint
@@ -328,21 +421,24 @@ function coerceOrderDict(oDict::Dict{String,Any}) #Also used in getOrder endpoin
     return oDict
 end
 
-
 """
 getOrders(config, count::Int=50; kwargs...)
 getOrders(config, IDlist::Vector; kwargs...)
 
-# Keyword Arguments
-    - 'state::String: a string with values "PENDING", "FILLED", "TRIGGERED", "CANCELLED" or "ALL". Defaults to "PENDING"
-    - 'instrument::String'
-    - 'beforeID::String': Last ID to retrieve
+#Arguments
+- 'config::config': a valid struct with user configuration data
+- 'count::Int': number of orders to return
+- 'IDlist::Vector{String}' : a vector of valid order IDs
+
+#Keyword Arguments
+- 'state::String: a string with values "PENDING", "FILLED", "TRIGGERED", "CANCELLED" or "ALL". Defaults to "PENDING"
+- 'instrument::String'
+- 'beforeID::String': last ID to retrieve
 
 #Examples
     getOrders(userData,5)
     getOrders(userData,25, state="FILLED", instrument="EUR_USD")
     getOrders(userData,["123","234","345"])
-
 """
 function getOrders(config, count::Int=50; kwargs...)
 
@@ -381,8 +477,7 @@ end
 getPendingOrders(config)
 
 #Examples
-    getPendingOrders(userData)
-  
+    getPendingOrders(userData)  
 """
 function getPendingOrders(config)
     
@@ -398,7 +493,6 @@ function getPendingOrders(config)
     return temp.orders
 end
 
-
 # ------------------------------------------------------------------------------------
 # /accounts/{accountID}/orders/{orderSpecifier} GET Endpoint
 # ------------------------------------------------------------------------------------
@@ -413,11 +507,14 @@ end
 JSON3.StructType(::Type{singleOrder}) = JSON3.Mutable()
 
 """
-getOrder(config, ID::Union{String,Int})
+getOrder(config, orderID)
+
+#Arguments
+- 'config::config': a valid struct with user configuration data
+- 'orderID::Union{String,Int}' : avalid order IDs
 
 #Examples
-    getOrder(userdata,"100")
-  
+    getOrder(userdata,"100")  
 """
 function getOrder(config, orderID::Union{String,Int})
     
@@ -441,7 +538,6 @@ end
 
    replaceOrder(userData,165,"LIMIT","EUR_JPY",100)
    replaceOrder(userData,170,"EUR_CHF",100,SL=(distance=0.1,),TP=(price=1.12,),tSL=(distance=0.3,))
-
 """
 function replaceOrder(config::config, orderID::Union{Int,String},type::String, instrument::String, units::Real, price::Real;
     TIF::String = "GTC", gtdTime::Union{Nothing,String}=nothing, positionFill::String = "DEFAULT", trigger::String="DEFAULT",priceBound::Union{Nothing,String}=nothing,
@@ -506,9 +602,12 @@ end
 """
 cancelOrder(config, ID::Union{String,Int})
 
+#Arguments
+- 'config::config': a valid struct with user configuration data
+- 'orderID::Union{String,Int}' : avalid order IDs
+
 #Examples
-    cancelOrder(userdata,"100")
-  
+    cancelOrder(userdata,"100")  
 """
 function cancelOrder(config, ID::Union{String,Int})
     
@@ -539,7 +638,6 @@ Lets add user information to a specific Trade
 # Example
 
     orderClientExtensions(userconfig,"66", clientID="007",tag="foo")
-
 """
 function orderClientExtensions(config, orderID::Union{Int,String}; clientID::String="", tag::String="", comment::String="")
 
